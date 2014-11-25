61ab240c5d1d1a9ae24d2faae1a7363c4ad84943
Nate Walck <n8@dropbox.com>
2014-11-25 10:13:19 -0500

    Added list display for admin pane

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.name
