2f1e41729c3131b93f293e78d3709c72d37b593f
Nate Walck <n8@dropbox.com>
2014-11-25 13:11:36 -0500

    Chapter 7 complete

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -12,39 +12,47 @@ def populate():
 
     add_page(cat=python_cat,
         title="Official Python Tutorial",
-        url="http://docs.python.org/2/tutorial/")
+        url="http://docs.python.org/2/tutorial/",
+        views=305)
 
     add_page(cat=python_cat,
         title="How to Think like a Computer Scientist",
-        url="http://www.greenteapress.com/thinkpython/")
+        url="http://www.greenteapress.com/thinkpython/",
+        views=105)
 
     add_page(cat=python_cat,
         title="Learn Python in 10 Minutes",
-        url="http://www.korokithakis.net/tutorials/python/")
+        url="http://www.korokithakis.net/tutorials/python/",
+        views=75)
 
     django_cat = add_cat("Django", 32, 64)
 
     add_page(cat=django_cat,
         title="Official Django Tutorial",
-        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/",
+        views=295)
 
     add_page(cat=django_cat,
         title="Django Rocks",
-        url="http://www.djangorocks.com/")
+        url="http://www.djangorocks.com/",
+        views=133)
 
     add_page(cat=django_cat,
         title="How to Tango with Django",
-        url="http://www.tangowithdjango.com/")
+        url="http://www.tangowithdjango.com/",
+        views=95)
 
     frame_cat = add_cat("Other Frameworks", 16, 32)
 
     add_page(cat=frame_cat,
         title="Bottle",
-        url="http://bottlepy.org/docs/dev/")
+        url="http://bottlepy.org/docs/dev/",
+        views=15)
 
     add_page(cat=frame_cat,
         title="Flask",
-        url="http://flask.pocoo.org")
+        url="http://flask.pocoo.org",
+        views=35)
 
     # Print out what we have added to the user.
     for c in Category.objects.all():
@@ -57,8 +65,8 @@ def add_page(cat, title, url, views=0):
 
 def add_cat(name, likes, views):
     c = Category.objects.get_or_create(name=name, likes=likes, views=views)[0]
-    c.views = views
-    c.likes = likes
+    # c.views = views
+    # c.likes = likes
     return c
 
 # Start execution here!
